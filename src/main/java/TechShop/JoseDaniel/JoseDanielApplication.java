620610dfec6c5fc8f70bb945bed7466a44da7f57
DanielbigM <danielmurillocr@hotmail.com>
2026-05-20 20:09:42 -0600

    Commit 1
    Creación del proyecto y Semana 1 y 2

---
+++ src/main/java/TechShop/JoseDaniel/JoseDanielApplication.java
@@ -0,0 +1,13 @@
+package TechShop.JoseDaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoseDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoseDanielApplication.class, args);
+	}
+
+}
